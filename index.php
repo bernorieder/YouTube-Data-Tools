b5f575f045f9903668168ae9f676ff23045ab3d7
Bernhard Rieder <rieder@MyMBA.local>
2015-05-05 09:48:20 +0200

    small suff

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
 	<td><h2>Video Info and Comments</h2></td>
 	<td>
 		<p>This module starts from a video id and retrieves basic info for the video in question and provides a number of analyses of the comment section.</p>
-		<p><a href="mod_video_comments.php">launch</a></p>
+		<p><a href="mod_video_info.php">launch</a></p>
 	</td>
 </tr>
 </table>
