4b9efdbadb712861d29492ca271757ee7b3ad59e
Bernhard Rieder <rieder@MyMBA.local>
2015-05-13 16:05:05 +0200

    text

--- faq.php
+++ faq.php
@@ -58,7 +58,7 @@
 		<p>Unfortunately, I do not have the spare time to provide any assistance for this app and can therefore not respond to inquiries concerning how to use the app or how
 		to solve a particular research problem with it.</p>
 		
-		<p>I will make an introductory video as soon as I have te time, however. In the meantime, the interface for
+		<p>There is an <a href="https://www.youtube.com/watch?v=sbErTW2MzCY" target="_blank">introductory video</a> and the interface for
 		each data module contains a description of what is does and links to the relevant sections of the API. Most importantly, to make sense of the data, a good
 		understanding of YouTube's basic architecture is required. The <a href="https://developers.google.com/youtube/v3/"  target="_blank">
 		documentation</a> for YouTube's API has comprehensive descriptions of entities and metrics.</p>
@@ -73,25 +73,29 @@
 		
 		<h2>What are channel or video ids and how can I find them?</h2>
 		
-		<p>Almost all of the modules require a video or channel id as input. These can normally be found in the respective YouTube URLs. For example, in the URL https://www.youtube.com/watch?v=BNM4kEUEcp8 the strange code after the "=" sign is the video id.
-		For further information, there's a lot of great information available on these things through a simple Google query.</p>
+		<p>Many of the modules require a video or channel id as input. These can normally be found in the respective YouTube URLs.
+		For example, in the URL https://www.youtube.com/watch?v=BNM4kEUEcp8 the strange code after the "=" sign is the video id.
+		For further information, there's a lot of great material available on these things through a simple Google query.</p>
 		
 		
 		<h2>The tool does not work (correctly)!</h2>
 
-		<p>While this is very simple software, this can happen for various reasons.</p>
+		<p>While this is very simple software, this can happen for all kinds of reasons.</p>
 
-		<p>High quality bug reports are much appreciated. If you have no experience with reporting bugs effectively, please read <a href="http://www.chiark.greenend.org.uk/~sgtatham/bugs.html" target="_blank">this piece</a> at least twice.
-		TL;DR: developers need context to debug a tool, when filing a bug report, please add the URL of the call, the browser you are using, a screenshot of the interface output,
-		the data files, and a description of what you have been doing and how the problem manifests itself.</p>
+		<p>High quality bug reports are much appreciated. If you have no experience with reporting bugs effectively, please read
+		<a href="http://www.chiark.greenend.org.uk/~sgtatham/bugs.html" target="_blank">this piece</a> at least twice.
+		TL;DR: developers need context to debug a tool, when filing a bug report, please add the URL of the call, the browser you are using, a
+		screenshot of the interface output, the data files, and a description of what you have been doing and how the problem manifests itself. Without extensive
+		information it can be very hard to replicate a problem and subsequently fix it.</p>
 		
 		<p>Please send bug reports to <a href="mailto:tools@polsys.net">tools@polsys.net</a>. I do not react to reports sent through any other channel.</p>
 		
 		
 		<h2>I want to make crawls with higher crawl depth!</h2>
 		
-		<p>Since the public version of the script runs on a server that does a bunch of things, this is not possible. But you can always get the source code (see below) and remove the line of code that checks for crawl depth. You may still run out of RAM, but networks with > 100K nodes should
-		be easily doable.</p>
+		<p>Since the public version of the script runs on a server that does a bunch of different things, this is not possible due to resource
+		constraints. But you can always get the source code (see below) and remove the line of code that checks for crawl depth. You may still run out of RAM,
+		but networks with > 100K nodes should be easily doable with 4 GB.</p>
 		
 		
 		<h2>Can you add feature X to YTDT?</h2>
@@ -99,9 +103,9 @@
 		<p>I cannot make any guarantees, but if you send a feature request to <a href="mailto:tools@polsys.net">tools@polsys.net</a>, I will definitely consider it.</p>
 		
 		
-		<h2>Can I have the source code?</h2>
+		<h2>Where is the source code?</h2>
 		
-		<p>Yes, you can. The full source code is available on <a href="https://github.com/bernorieder/YouTube-Data-Tools" target="_blank">github</a>. You'll also find installation instructions there.</p>
+		<p>The full source code is available on <a href="https://github.com/bernorieder/YouTube-Data-Tools" target="_blank">github</a>. You'll also find installation instructions there.</p>
 	</td>
 </tr>
 </table>
