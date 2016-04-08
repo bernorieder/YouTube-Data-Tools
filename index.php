f903f60612de7d1edbda04d10f5a4aa55e1d65f2
Bernhard Rieder <berno.rieder@gmail.com>
2016-04-08 09:26:02 +0200

    text updates

--- index.php
+++ index.php
@@ -95,6 +95,7 @@
 	<td colspan="2">
 		<h2>Version History</h2>
 		<ul>
+			<li>v1.04 - 01/02/2015 - Added like/dislike ratio to videonet</li>
 			<li>v1.03 - 18/10/2015 - Small bugfix</li>
 			<li>v1.02 - 05/06/2015 - Added subscriptions to channel network</li>
 			<li>v1.01 - 26/05/2015 - Bug fix for video network, video category added to several modules</li>
