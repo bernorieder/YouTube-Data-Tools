9cf99fafa26259afa0e800b12b7e50f1bac47b4f
Bernhard Rieder <berno.rieder@gmail.com>
2016-04-08 09:46:35 +0200

    text updates

--- mod_channels_net.php
+++ mod_channels_net.php
@@ -43,7 +43,7 @@ require_once "common.php";
 				
 				<p>Crawl depth specifies how far from the seeds the script should go. Crawl depth 0 will get only the relations between seeds. Using many seeds and the maximum crawl depth (2) can take a very long time or the script might run out of memory. Start small.</p>
 				
-				<p>NB: since graph analysis software can have difficulties with very large numbers, channel's viewcount is given in 100s.</p>
+				<p>NB: since graph analysis software can have difficulties with very large numbers, channels' viewcount is given in 100s.</p>
 			</td>
 		</tr>
 		<tr>
