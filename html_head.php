0a59c1525ebcc3efd01ceaff4c991d49511ee8be
Bernhard Rieder <berno.rieder@gmail.com>
2018-06-05 15:00:50 +0200

    latest version

--- html_head.php
+++ html_head.php
@@ -21,4 +21,19 @@ require_once "common.php";
 <body>
 
 <div id="fullpage">
+
+
+	
+	
+	<div class="rowTab">
+		<div class="sectionTab">
+			<a href="index.php" class="navlink">Home</a>
+			<a href="mod_channel_info.php" class="navlink">Channel Info</a>
+			<a href="mod_channels_net.php" class="navlink">Channel Network</a>
+			<a href="mod_videos_list.php" class="navlink">Video List</a>
+			<a href="mod_videos_net.php" class="navlink">Video Network</a>
+			<a href="mod_video_info.php" class="navlink">Video Info</a>
+			<a href="faq.php" class="navlink">FAQ</a>
+		</div>
+	</div>
 	
